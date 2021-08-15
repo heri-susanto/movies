e10e8f4860f19b891adae3b076817cf4125e49e4
Heri Susanto <heri@bangunindo.com>
2021-08-15 16:03:05 +0700

    hehe

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: false,
+    async rewrites() {
+      return [
+        {
+          source: "/:slug*.html", // Old url with .html
+          destination: "/:slug*", // Redirect without .html
+        },
+      ];
+    },
 };
